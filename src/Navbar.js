1ec4e39cbcef1df4ef65bc7ef36f3e21debd8d04
JasonBMa <JasonBMa@users.noreply.github.com>
2023-10-03 17:28:32 -0700

    Removed some files added some code, started use of Tailwind.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,38 +2,42 @@ import React from 'react';
 import {BsFillPersonFill} from "react-icons/bs";
 import {BsBarChartLine} from "react-icons/bs";
 import {BsBasket} from "react-icons/bs";
+import { useState } from 'react';
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className="bg-green-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="bg-green-500">
+      <div className="container flex w-full items-center justify-between">
         <a className="text-white text-2xl font-bold" href="/">
-          My Website
+          Bicycle Exchange
         </a>
-        <ul className="space-x-4 block">
-          <li>
-            <img src={BsBasket} alt="checkout"></img>
+        <div className="flex items-center justify-end">
+          <div class="flex items-center">
+            <BsBasket/>
             <a href="/" className="text-white hover:underline">Cart</a>
-          </li>
-          <li>
-            <button className="text-white hover:underline">Name</button>
-            <div className="dropdown-menu">
-              <ul className="space-x-4">
-                <DropdownItem img={<BsFillPersonFill/>} text={"Profile"}/>
-                <DropdownItem img={<BsBarChartLine/>} text={"Settings"}/>
-              </ul>
-            </div>
-          </li>
-        </ul>
+          </div>
+          <div class="">
+            <button className="text-white hover:underline" onClick={() => setToggle((prev)=>!(prev))}>
+              Name
+            </button>
+          </div>
+        </div>
       </div>
+      {toggle && <div className='dropdown-menu'>
+        <ul className="list-none space-x-4">
+          <DropdownItem img={<BsFillPersonFill/>} text={"Profile"}/>
+          <DropdownItem img={<BsBarChartLine/>} text={"Settings"}/>
+        </ul>
+      </div>}
     </nav>
   );
 }
 
-function DropdownItem(props){
+function DropdownItem(args){
   return(
-    <li className= "dropdownItem">
-      <div className="block">
-      <a href="/">{props.img} {props.text}</a>
+    <li className="dropdownItem">
+      <div>
+      <a className="flex" href="/">{args.img} {args.text}</a>
       </div>
     </li>
   );
